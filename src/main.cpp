406072743a706dca78f25edf4f6c5db48e30ea1b
@ivan_xoxol <ivan_mischenko@niuitmo.ru>
2025-06-17 15:46:45 +0300

    chng sensor lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,21 @@
+#include <Wire.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <Wire.h>
-#include "Adafruit_VL53L0X.h"
+#include <VL53L0X.h>
 
 #define MODULE_TYPE "rack-module"
-
-// --- WiFi ---
-const char* ssid = "Beeline_MF";
-const char* password = "$@ndr0nix";
-
-// --- MQTT ---
-const char* mqtt_server = "192.168.8.100"; // IP Orange Pi
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-// --- Мультиплексоры ---
 #define MUX1_ADDR 0x70
 #define MUX2_ADDR 0x77
 
-// 10 датчиков по порядку: 0–4 на первом MUX, 0–4 на втором
-Adafruit_VL53L0X sensor;
+const char* ssid = "Beeline_MF";
+const char* password = "$@ndr0nix";
+const char* mqtt_server = "192.168.8.100";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+VL53L0X sensors[10];
+
 uint8_t muxAddr[] = { MUX2_ADDR, MUX2_ADDR, MUX2_ADDR, MUX2_ADDR, MUX2_ADDR,
                       MUX1_ADDR, MUX1_ADDR, MUX1_ADDR, MUX1_ADDR, MUX1_ADDR };
 uint8_t channel[] = {4, 3, 2, 1, 0, 0, 1, 2, 3, 4};
@@ -33,8 +29,6 @@ void readAndSendSensors();
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
-  Serial.println("Start monitor");
   Wire.begin();
 
   setup_wifi();
@@ -50,34 +44,29 @@ void loop() {
   client.loop();
 
   readAndSendSensors();
-  delay(1000);
+  delay(1500);
 }
 
-// Функция выбора канала мультиплексора
 void selectMux(uint8_t muxAddr, uint8_t channel) {
   Wire.beginTransmission(muxAddr);
   Wire.write(1 << channel);
   Wire.endTransmission();
+  delay(50);
 }
 
-// Подключение к WiFi
 void setup_wifi() {
-  delay(1000);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nWiFi connected");
-  Serial.println("\nIP: " + String(WiFi.localIP().toString()));
-  Serial.println("\nMAC: " + String(WiFi.macAddress()));
+  Serial.println(WiFi.localIP());
 }
 
-// Подключение к MQTT
 void reconnect() {
   while (!client.connected()) {
-    if (client.connect((WiFi.macAddress()).c_str())) {
-      Serial.println("Connected to MQTT");
+    if (client.connect(WiFi.macAddress().c_str())) {
       sendIdentity();
     } else {
       delay(1000);
@@ -85,7 +74,6 @@ void reconnect() {
   }
 }
 
-// Отправка информации о модуле
 void sendIdentity() {
   String ip = WiFi.localIP().toString();
   String mac = WiFi.macAddress();
@@ -94,34 +82,39 @@ void sendIdentity() {
   client.publish(("module/" + mac + "/identity/ip").c_str(), ip.c_str());
 }
 
-// Настройка датчиков
 void initSensors() {
   for (int i = 0; i < 10; i++) {
     selectMux(muxAddr[i], channel[i]);
-    if (!sensor.begin()) {
-      Serial.printf("Sensor %d not found\n", i+1);
+    delay(100);
+    sensors[i].setTimeout(500);
+    if (!sensors[i].init()) {
+      Serial.printf("Sensor %d not found\n", i + 1);
+      continue;
     } else {
-      Serial.printf("Sensor %d OK\n", i+1);
+      // Включаем Long Range Mode
+      sensors[i].setSignalRateLimit(0.1);
+      sensors[i].setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+      sensors[i].setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
     }
   }
 }
 
-// Отправка показаний
 void readAndSendSensors() {
+  String mac = WiFi.macAddress();
+
   for (int i = 0; i < 10; i++) {
     selectMux(muxAddr[i], channel[i]);
-
-    VL53L0X_RangingMeasurementData_t measure;
-    sensor.rangingTest(&measure, false);
-
-    if (measure.RangeStatus != 4) {
-      char topic[64];
-      snprintf(topic, sizeof(topic), "module/%s/sensor/vl53l0x/%d", (WiFi.macAddress()).c_str(), i+1);
-      char payload[16];
-      snprintf(payload, sizeof(payload), "%d", measure.RangeMilliMeter);
-      client.publish(topic, payload);
+    delay(100);
+    uint16_t dist = sensors[i].readRangeSingleMillimeters();
+    if (sensors[i].timeoutOccurred()) {
+      Serial.printf("Sensor %d timeout\n", i + 1);
+      continue;
     }
 
-    delay(50);
+    char topic[64];
+    snprintf(topic, sizeof(topic), "module/%s/sensor/vl53l0x/%d", mac.c_str(), i + 1);
+    char payload[16];
+    snprintf(payload, sizeof(payload), "%d", dist);
+    client.publish(topic, payload);
   }
 }
